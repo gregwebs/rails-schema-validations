635f0bfe9ff1e49e0bce02daf71e2ff68a72f7a9
Greg Weber <greg@gregweber.info>
2010-09-22 20:38:37 -0700

    set allow_nil option if column can be NULL

--- lib/validations_from_schema.rb
+++ lib/validations_from_schema.rb
@@ -4,15 +4,17 @@ module ActiveRecord::Validations::ClassMethods
     self.content_columns.map do |col|
       case col.type
       when :integer
-        validates_numericality_of col.name, :only_integer => true
+        validates_numericality_of col.name, :only_integer => true, :allow_nil => col.null
       when :float
-        validates_numericality_of col.name
+        validates_numericality_of col.name, :allow_nil => col.null
       #when :time, :datetime :string 
       end
     end.compact + content_columns.map do |col|
       validates_presence_of col.name unless col.null
     end.compact + content_columns.map do |col|
-      validates_length_of col.name, :maximum => col.limit if col.limit
+      if col.limit
+        validates_length_of col.name, :maximum => col.limit, :allow_nil => col.null
+      end
     end
   end
 end
